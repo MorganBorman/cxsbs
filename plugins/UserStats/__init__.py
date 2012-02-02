d8568950cd38cc2615d53330786903c9b1002c41
morgan <morgan.borman@gmail.com>
2012-02-01 18:48:47 -0800

    Moved the writing of the user stats out to a new thread to eliminate the lag issues.

--- plugins/UserStats/__init__.py
+++ plugins/UserStats/__init__.py
@@ -5,7 +5,15 @@ class Plugin(cxsbs.Plugin.Plugin):
 		cxsbs.Plugin.Plugin.__init__(self)
 		
 	def load(self):
-		pass
+
+		statWriter = StatWriter()
+		statWriter.start()
+		
+		Events.registerServerEventHandler('player_teamkill', statWriter.on_teamkill)
+		Events.registerServerEventHandler('player_frag', statWriter.on_frag)
+		Events.registerServerEventHandler('player_suicide', statWriter.on_suicide)
+		Events.registerServerEventHandler('player_spend_damage', statWriter.on_shot)
+		Events.registerServerEventHandler('player_inflict_damage', statWriter.on_hit)
 		
 	def unload(self):
 		pass
@@ -17,7 +25,7 @@ from sqlalchemy.orm import relation, mapper
 from sqlalchemy.schema import UniqueConstraint
 from sqlalchemy.sql.expression import func
 
-import datetime
+import datetime, threading
 
 Base = declarative_base()
 
@@ -133,6 +141,53 @@ def AccessCurrentStat(session, userId):
 		session.commit()
 		return stat
 
+class StatWriter(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.running = True
+		self.event_queue = []
+		self.flag = threading.Event()
+		
+	def run(self):
+		while self.running:
+			
+			self.flag.clear()
+			self.flag.wait()
+			
+			while len(self.event_queue) > 0:
+				event = self.event_queue.pop(0)
+				#do something with it
+				try:
+					event[0](*(event[1]))
+				except:
+					pass
+				
+	def stop(self):
+		self.running = False
+		self.flag.set()
+		
+	def on_teamkill(self, cn, tcn):
+		self.event_queue.append((on_teamkill, (cn, tcn)))
+		self.event_queue.append((on_death, (tcn,)))
+		self.flag.set()
+	
+	def on_frag(self, cn, tcn):
+		self.event_queue.append((on_frag, (cn, tcn)))
+		self.event_queue.append((on_death, (tcn,)))
+		self.flag.set()
+	
+	def on_suicide(self, cn):
+		self.event_queue.append((on_suicide, (cn,)))
+		self.flag.set()
+	
+	def on_shot(self, cn, damage):
+		self.event_queue.append((on_shot, (cn, damage)))
+		self.flag.set()
+	
+	def on_hit(self, cn, damage):
+		self.event_queue.append((on_hit, (cn, damage)))
+		self.flag.set()
+
 """
 @Events.eventHandler('player_disconnect')
 def onPlayerDisconnect(cn):
@@ -161,7 +216,7 @@ def onPlayerConnect(cn):
 		session.close()
 """
 
-def onDeath(cn):
+def on_death(cn):
 	if not Users.isLoggedIn(cn):
 		return
 	user = Players.player(cn)
@@ -174,9 +229,7 @@ def onDeath(cn):
 	finally:
 		session.close()
 
-@Events.eventHandler('player_teamkill')
-def onTeamkill(cn, tcn):
-	onDeath(tcn)
+def on_teamkill(cn, tcn):
 	if not Users.isLoggedIn(cn):
 		return
 	user = Players.player(cn)
@@ -188,10 +241,8 @@ def onTeamkill(cn, tcn):
 		session.commit()
 	finally:
 		session.close()
-	
-@Events.eventHandler('player_frag')
-def onFrag(cn, tcn):
-	onDeath(tcn)
+
+def on_frag(cn, tcn):
 	if not Users.isLoggedIn(cn):
 		return
 	user = Players.player(cn)
@@ -204,9 +255,7 @@ def onFrag(cn, tcn):
 	finally:
 		session.close()
 
-@Events.eventHandler('player_suicide')
-def onSuicide(cn):
-	onDeath(cn)
+def on_suicide(cn):
 	if not Users.isLoggedIn(cn):
 		return
 	user = Players.player(cn)
@@ -252,8 +301,7 @@ def onShotHit(cn, tcn, lifesequence, dist, hitrays):
 		session.close()
 """
 
-@Events.eventHandler('player_spend_damage')
-def onShot(cn, damage):
+def on_shot(cn, damage):
 	if not Users.isLoggedIn(cn):
 		return
 	
@@ -267,8 +315,7 @@ def onShot(cn, damage):
 	finally:
 		session.close()
 		
-@Events.eventHandler('player_inflict_damage')
-def onShotHit(cn, damage):
+def on_hit(cn, damage):
 	if not Users.isLoggedIn(cn):
 		return
 	
@@ -282,7 +329,3 @@ def onShotHit(cn, damage):
 		#ServerCore.message(("Current accuracy: %0.2f" %(stat.accuracy()*100)) + '%')
 	finally:
 		session.close()
-
-@Events.eventHandler('intermission_begin')
-def onIntermission():
-	pass
