92f467ffdae7fc76655c416ca1def2c49707d14f
Morgan Borman <morgan@morgan-EP45-UD3P.(none)>
2012-02-22 17:20:03 -0800

    Updated enet to r1600 from sauerbraten svn and, up stray files, and improved .gitignore files.

---
+++ testFramework/drop_stat_tables.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS usermanager_connection_history;
+DROP TABLE IF EXISTS usermanager_damage_dealt_events;
+DROP TABLE IF EXISTS usermanager_damage_spent_events;
+DROP TABLE IF EXISTS usermanager_death_events;
+DROP TABLE IF EXISTS usermanager_frag_events;
